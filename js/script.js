2b2f129f57adc3a2e492294755df97076626e0ac
BeneArp <benedetta.arpino@gmail.com>
2024-06-11 13:18:34 +0200

    feat: add while cycle

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ const contenitoreLista = document.getElementById("lista-della-spesa");
 // creo un array con gli alimenti da comprate
 const listaSpesa = ["pane", "pasta", "pomodoro", "mozzarella", "mele", "melanzane"];
 
+
+// creo un ciclo while che legga tutti gli elementi dell'arrey e li stampi in pagina
+
+let i = 0;
+
+while(i < listaSpesa.length){
+
+    let alimentoLista = listaSpesa[i];
+    // stampo in console tutto il contenuto dell'array
+    console.log(alimentoLista);
+
+    // creo una variabile con l'html da inserie nella ul del dom
+    let elementoLista = `<li>${alimentoLista}</li>`
+
+    i++;
+}
