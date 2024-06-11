3d1bdf670d12b5445acba5bb311e4dac38128844
BeneArp <benedetta.arpino@gmail.com>
2024-06-11 13:17:10 +0200

    feat: add dom reference to ul

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 // Consegna:
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
-console.log("ciao");
+// prendo dal dom il riferimento al ul che conterra i li
+const contenitoreLista = document.getElementById("lista-della-spesa");
+
